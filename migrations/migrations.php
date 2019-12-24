fb3003c92a65ab399c714a570a2710c5b26f0769
Andres Agudelo <andres.agudelo@cerok.com>
2019-12-24 11:57:34 -0500

    Se configura migraciones

--- migrations/migrations.php
+++ migrations/migrations.php
@@ -2,12 +2,12 @@
 
 return [
     'name' => 'SAIA Migrations',
-    'migrations_namespace' => 'SAIA\Migrations\Pqr',
+    'migrations_namespace' => 'Saia\Pqr\Migrations',
     'table_name' => 'migrations_pqr',
     'column_name' => 'version',
     'column_length' => 14,
     'executed_at_column_name' => 'executed_at',
     'migrations_directory' => 'list',
     'all_or_nothing' => true,
-    'check_database_platform' => true,
+    'check_database_platform' => true
 ];
