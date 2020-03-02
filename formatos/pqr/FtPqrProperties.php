5fc4ee301ae2098c082e3ca7c4ef352d8c50d808
Andres Agudelo <andres92-agudelo@outlook.com>
2020-03-01 19:35:55 -0500

    Se quita del ignore a propieties

---
+++ formatos/pqr/FtPqrProperties.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Saia\Pqr\formatos\pqr;
+
+use Saia\core\model\ModelFormat;
+
+class FtPqrProperties extends ModelFormat
+{
+    public function __construct($id = null)
+    {
+        parent::__construct($id);
+    }
+
+    protected function defaultDbAttributes()
+    {
+        return [
+            'safe' => [
+                'dependencia',
+				'documento_iddocumento',
+				'encabezado',
+				'firma',
+				'idft_pqr',
+				'nombre',
+				'sys_email',
+				'sys_estado',
+				'sys_tipo' 
+            ],
+            'date' => [],
+            'table' => 'ft_pqr',
+            'primary' => 'idft_pqr'
+        ];
+    }
+
+    protected function defineMoreAttributes()
+    {
+        return [];
+    }
+}
