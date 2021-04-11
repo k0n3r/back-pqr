482d941e4398d482634b63bdbef6cb2f9822f88e
Andres Agudelo <andres92-agudelo@outlook.com>
2021-04-11 11:00:13 -0500

    Se crea migracion para eliminar el cteondido del campo descripcion de la respueta pqr

---
+++ Resources/migrations/Version20210411151159.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bundles\pqr\Resources\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210411151159 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Se quita tipo descripcion en campo contenido de respuesta pqr';
+    }
+
+    private function getFormatId()
+    {
+        $sql = "select idformato from formato where nombre='pqr_respuesta'";
+        return $this->connection->fetchOne($sql);
+    }
+
+    public function up(Schema $schema): void
+    {
+        $formatId = $this->getFormatId();
+
+        $this->connection->update('campos_formato', [
+            'acciones' => 'a,e'
+        ], [
+            'nombre' => 'contenido',
+            'formato_idformato' => $formatId
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $formatId = $this->getFormatId();
+
+        $this->connection->update('campos_formato', [
+            'acciones' => 'p,a,e'
+        ], [
+            'nombre' => 'contenido',
+            'formato_idformato' => $formatId
+        ]);
+    }
+}
