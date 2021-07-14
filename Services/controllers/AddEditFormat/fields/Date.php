1d51d43dd4bf5ff7bf2454b9c7d7dbb8d63c1ec6
k0n3r <andres92-agudelo@outlook.com>
2021-07-13 20:50:02 -0500

    Se adiciona el campo tipo fecha

---
+++ Services/controllers/AddEditFormat/fields/Date.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Bundles\pqr\Services\controllers\AddEditFormat\fields;
+
+class Date extends Field implements IField
+{
+    use TField;
+
+    /**
+     * @inheritDoc
+     */
+    public function getValues(): array
+    {
+        $PqrFormField = $this->getPqrFormField();
+        $type = $PqrFormField->getSetting()->dateType;
+
+        $data = array_merge($this->getDefaultValues(), [
+            'tipo_dato'   => 'datetime',
+            'placeholder' => $PqrFormField->getSetting()->placeholder,
+            'opciones'    => '{"hoy":true,"tipo":"' . $type . '"}'
+        ]);
+
+        if (!$PqrFormField->active) {
+            $data['etiqueta_html'] = 'Hidden';
+            $data['opciones'] = '{"type":"hidden"}';
+        }
+
+        return $data;
+    }
+}
