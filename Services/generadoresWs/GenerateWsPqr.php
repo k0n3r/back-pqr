895ed652ee075251f56dd6f7a3d34a52fef3109c
Andres Agudelo <andres.agudelo@cerok.com>
2023-07-13 10:45:43 -0500

    ajuste generador ws pqr

--- Services/generadoresWs/GenerateWsPqr.php
+++ Services/generadoresWs/GenerateWsPqr.php
@@ -9,7 +9,29 @@ use Saia\controllers\generator\webservice\WsGenerator;
 
 class GenerateWsPqr extends GenerateWsFt
 {
+    protected function getGenerateSearch(): bool
+    {
+        return false;
+    }
+
     public function generate(): void
+    {
+        $PqrForm = PqrForm::getInstance();
+
+        $WsGenerator = new WsGenerator(
+            $PqrForm->getWebservicePqr(),
+            $this->getNameFormat(),
+            $this->getGenerateSearch()
+        );
+
+        $this->createFiles($WsGenerator);
+
+        if (!$WsGenerator->create()) {
+            throw new SaiaException("No fue posible generar el ws: {$this->Formato->etiqueta}");
+        }
+    }
+
+    protected function createFiles(WsGenerator $WsGenerator): void
     {
         $folder = 'src/Bundles/pqr/Services/controllers/templates/';
         $page404 = WsGenerator::generateFileForWs('src/legacy/controllers/generator/webservice/templates/404.html');
@@ -17,18 +39,6 @@ class GenerateWsPqr extends GenerateWsFt
         $infoQRJsFile = WsGenerator::generateFileForWs($folder . 'infoQR.js');
         $timelineFile = WsGenerator::generateFileForWs($folder . 'TimeLine.js');
 
-        $PqrForm = PqrForm::getInstance();
-
-        $WsGenerator = new WsGenerator(
-            $PqrForm->getWebservicePqr(),
-            $this->Formato->nombre,
-            false
-        );
-
         $WsGenerator->addFiles([$infoQrFile, $infoQRJsFile, $timelineFile, $page404]);
-
-        if (!$WsGenerator->create()) {
-            throw new SaiaException("No fue posible generar el ws: {$this->Formato->etiqueta}");
-        }
     }
 }
