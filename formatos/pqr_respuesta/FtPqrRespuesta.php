f8f7cad9250a41cd3a261d226a04c0f2bbe4fbf9
Andres Agudelo <andres.agudelo@cerok.com>
2023-11-08 18:27:10 -0500

    se refactoriza la respuesta para poder extender y modificar

--- formatos/pqr_respuesta/FtPqrRespuesta.php
+++ formatos/pqr_respuesta/FtPqrRespuesta.php
@@ -6,6 +6,7 @@ use App\Bundles\pqr\helpers\UtilitiesPqr;
 use App\Bundles\pqr\Services\FtPqrRespuestaService;
 use Exception;
 use Saia\controllers\localidad\MunicipioService;
+use Saia\models\anexos\Anexos;
 use Saia\models\Tercero;
 use Saia\models\localidades\Municipio;
 use App\Bundles\pqr\formatos\pqr\FtPqr;
@@ -195,15 +196,15 @@ HTML;
 
         return <<<HTML
             <div class='form-group form-group-default required'>
-                <label>{$CamposFormato->etiqueta}</label>
+                <label>$CamposFormato->etiqueta</label>
                 <div class='radio radio-success input-group'>
-                    <input type='radio' name='{$CamposFormato->nombre}' id='{$CamposFormato->nombre}1' value='1' $checked1>
+                    <input type='radio' name='$CamposFormato->nombre' id='{$CamposFormato->nombre}1' value='1' $checked1>
                     <label for='{$CamposFormato->nombre}1' class='mr-3 label-without-focus'> SI </label>
             
-                    <input type='radio' name='{$CamposFormato->nombre}' id='{$CamposFormato->nombre}0' value='0' $checked0>
+                    <input type='radio' name='$CamposFormato->nombre' id='{$CamposFormato->nombre}0' value='0' $checked0>
                     <label for='{$CamposFormato->nombre}0' class='mr-3 label-without-focus'> NO </label>
                 </div>
-                <label id='{$CamposFormato->nombre}-error' class='error' for='{$CamposFormato->nombre}' style='display: none;'></label>
+                <label id='$CamposFormato->nombre-error' class='error' for='$CamposFormato->nombre' style='display: none;'></label>
             </div>
 HTML;
     }
@@ -219,15 +220,14 @@ HTML;
      */
     public function showTemplate(): string
     {
-        $Qr = CoreFunctions::mostrar_qr($this);
-        $firmas = CoreFunctions::mostrar_estado_proceso($this);
-        $Service = $this->getService();
-        $contenido = $this->getFieldValue('contenido');
+        $Qr = $this->showQr();
+        $firmas = $this->showSignatures();
+
         return <<<HTML
             <table style="width: 100%;border: 0">
                 <tbody>
                     <tr>
-                        <td colspan="2">{$Service->getFechaCiudad()}</td>
+                        <td colspan="2">{$this->getFechaCiudad()}</td>
                     </tr>
 
                     <tr>
@@ -241,8 +241,8 @@ HTML;
                     </tr>
 
                     <tr>
-                        <td>{$Service->getModel()->getFieldValue('destino')}</td>
-                        <td style="text-align:center">$Qr<br/>No.{$Service->getRadicado()}</td>
+                        <td>{$this->getFieldValue('destino')}</td>
+                        <td style="text-align:center">$Qr<br/>No.{$this->getRadicado()}</td>
                     </tr>
 
                     <tr>
@@ -272,12 +272,159 @@ HTML;
                     </tr>
                 </tbody>
             </table>
-            $contenido
-            <p>{$Service->getDespedida()}<br/><br/></p>
+            {$this->getContent()}
+            <p>{$this->getDespedida()}<br/><br/></p>
             $firmas
-            <p>{$Service->getOtherData()}</p>
+            <p>{$this->getOtherData()}</p>
 HTML;
 
+    }
+
+    protected function showQr()
+    {
+        return CoreFunctions::mostrar_qr($this);
+    }
+
+    protected function showSignatures()
+    {
+        return CoreFunctions::mostrar_estado_proceso($this);
+    }
+
+    protected function getContent(): string
+    {
+        return $this->getFieldValue('contenido') ?? '';
+    }
+
+    /**
+     * Obtiene fecha y nombre de la ciudad
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    public function getFechaCiudad(): string
+    {
+        return $this->getMunicipio()->nombre . ", " . strftime("%d de %B de %Y",
+                strtotime($this->getDocument()->fecha));
+    }
+
+    /**
+     * Obtiene el radicado
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    public function getRadicado(): string
+    {
+        return $this->getDocument()->getService()->getFilingReferenceNumber();
+    }
+
+    /**
+     * Obtiene el texto de despedida
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    public function getDespedida(): string
+    {
+        return $this->getKeyField('despedida') == FtPqrRespuesta::OTRA_DESPEDIDA ?
+            $this->otra_despedida : $this->getFieldValue('despedida');
+    }
+
+    /**
+     * Obtiene mas informacion que va en el mostrar
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    public function getOtherData(): string
+    {
+        $data = '';
+        if ($this->anexos_fisicos) {
+            $data .= "Anexos físicos: $this->anexos_fisicos<br/>";
+        }
+
+        if ($anexosDigitales = $this->getNameAnexosDigitales()) {
+            $data .= "Anexos digitales: $anexosDigitales<br/>";
+        }
+
+        $dataCopy = [];
+        if ((int)$this->ver_copia) {
+            if ($copia = $this->getFieldValue('copia_interna')) {
+                $dataCopy[] = $copia;
+            }
+        }
+
+        if ($copiaExterna = $this->getNameCopiaExterna()) {
+            $dataCopy[] = $copiaExterna;
+        }
+
+        if ($dataCopy) {
+            $infoCopy = implode(', ', $dataCopy);
+            $data .= "Con copia: $infoCopy<br/>";
+        }
+
+        $data .= "Proyectó: {$this->getCreador()}";
+
+        return $data;
+    }
+
+    /**
+     * Obtiene los nombres de los anexos
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    protected function getNameAnexosDigitales(): string
+    {
+        $id = $this->getFormat()->getField('anexos_digitales')->getPK();
+
+        $names = Anexos::findColumn('etiqueta', [
+            'documento_iddocumento' => $this->getDocument()->getPK(),
+            'campos_formato'        => $id,
+            'estado'                => 1,
+            'eliminado'             => 0
+        ]);
+
+        return $names ? implode(', ', $names) : '';
+    }
+
+    /**
+     * Obtiene los nombres de las personas a quien va con copia externa
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    protected function getNameCopiaExterna(): string
+    {
+        if (!$this->copia) {
+            return '';
+        }
+
+        $names = [];
+        $records = explode(',', $this->copia);
+        foreach ($records as $destino) {
+            $names[] = (new Tercero($destino))->nombre;
+        }
+
+        return implode(', ', $names);
+    }
+
+    /**
+     * Obtiene el nombre del creador o de quien proyectó
+     *
+     * @return string
+     * @author Andres Agudelo <andres.agudelo@cerok.com>
+     * @date   2020
+     */
+    protected function getCreador(): string
+    {
+        return $this->getDocument()->getMaker()->getName();
     }
 
 }
