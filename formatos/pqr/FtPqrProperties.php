999b92a6d6356f05c9f2157181dcaae7da831351
Andres Agudelo <andres.agudelo@cerok.com>
2024-07-05 10:22:09 -0500

    ajuste cache de pdf

--- formatos/pqr/FtPqrProperties.php
+++ formatos/pqr/FtPqrProperties.php
@@ -14,133 +14,104 @@ use Saia\models\ruta\RutaFormato;
 class FtPqrProperties extends ModelFormat
 {
     use DistributionExecutor;
-    
+
     public bool $isPDF = false;
 
     /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     protected function defaultDbAttributes(): array
     {
         return [
-            'safe' => [
+            'safe'    => [
                 'idft_pqr',
-				'documento_iddocumento',
-				'sys_estado',
-				'sys_tercero',
-				'sys_fecha_vencimiento',
-				'sys_fecha_terminado',
-				'sys_anonimo',
-				'sys_frecuencia',
-				'sys_impacto',
-				'sys_severidad',
-				'sys_oportuno',
-				'radicacion',
-				'dependencia',
-				'sys_tipo',
-				'encabezado',
-				'sys_email',
-				'sys_folios',
-				'firma',
-				'sys_anexos',
-				'sys_tratamiento',
-				'sys_dependencia',
-				'radio',
-				'sys_subtipo',
-				'ciudad',
-				'distribucion',
-				'destino_interno',
-				'select_mensajeria',
-				'descripcion',
-				'colilla',
-				'digitalizacion' 
+                'documento_iddocumento',
+                'sys_estado',
+                'sys_tercero',
+                'sys_fecha_vencimiento',
+                'sys_fecha_terminado',
+                'sys_anonimo',
+                'sys_frecuencia',
+                'sys_impacto',
+                'sys_severidad',
+                'sys_oportuno',
+                'radicacion',
+                'dependencia',
+                'sys_tipo',
+                'encabezado',
+                'sys_email',
+                'sys_folios',
+                'firma',
+                'sys_anexos',
+                'sys_tratamiento',
+                'sys_dependencia',
+                'radio',
+                'sys_subtipo',
+                'ciudad',
+                'distribucion',
+                'destino_interno',
+                'select_mensajeria',
+                'descripcion',
+                'colilla',
+                'digitalizacion'
             ],
-            'date' => ['sys_fecha_vencimiento',
-				'sys_fecha_terminado'],
-            'table' => 'ft_pqr',
+            'date'    => [
+                'sys_fecha_vencimiento',
+                'sys_fecha_terminado'
+            ],
+            'table'   => 'ft_pqr',
             'primary' => 'idft_pqr'
         ];
-    }
-
-    /**
-    * @inheritDoc
-    */
-    protected function defineMoreAttributes(): array
-    {
-        return [];
-    }
-    
-    /**
-    * @inheritDoc
-    */
-    public static function getParamsToAddEdit(int $action, int $idft): array
-    {
-        return [];
     }
 
     public function defaultDocumentRoute(): bool
     {
         $RutaFormato = new RutaFormato();
         $RutaFormato->addDefaultRouteFormat(
-            $this->getFormat()->getPk(), 
+            $this->getFormat()->getPk(),
             $this->getDocument()->getPk()
         );
 
         return true;
     }
-    
+
     /**
-    * @inheritDoc
-    */
-    public function getNumberFolios(): int
-    {
-        $Documento = $this->getDocument();
-
-        if ($Documento->numero_folios) {
-            $total = $Documento->numero_folios;
-        } else {
-            $total = ($this->numero_folios ?? 0);
-        }
-
-        return (int)$total;
-    }
-        
-        /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     public function afterRad(): bool
     {
         $existInPack = PaqueteDocumento::fromPackage($this->getDocument()->getPK());
         if (!$this->radicacion_rapida) {
-            $this->postDocumentRad();
             if ($existInPack == 0) {
                 if (!$this->sendDocumentsByEmail()) {
                     throw new SaiaException('No fue posible enviar la notificacion por correo');
                 }
             }
+            $this->postDocumentRad();
         }
 
         return true;
     }
 
     /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     public function afterEdit(): bool
     {
-         $Documento = $this->getDocument();
-         
-        if (!$this->editDistribution()){
-              throw new SaiaException('No fue posible editar la distribución');
+        $Documento = $this->getDocument();
+
+        if (!$this->editDistribution()) {
+            throw new SaiaException('No fue posible editar la distribución');
         }
-        
+
         if (
             $Documento->isStarted() &&
             $this->getFormat()->isAutoApprove()
         ) {
             $Documento->estado = Documento::APROBADO;
+            $Documento->estado_aprobacion = Documento::APROBADO_LABEL;
             $Documento->save();
-            
+
             if (!$this->sendDocumentsByEmail()) {
                 throw new SaiaException('No fue posible enviar la notificacion por correo');
             }
