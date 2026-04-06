c06cfc6c661d3313b4dc26f000c2489f1e32c343
Andres Agudelo <andres.agudelo@saiasoftware.com>
2026-04-06 16:25:06 -0500

    ajuste properties

--- formatos/pqr/FtPqrProperties.php
+++ formatos/pqr/FtPqrProperties.php
@@ -2,13 +2,10 @@
 
 namespace App\Bundles\pqr\formatos\pqr;
 
+use Saia\core\model\ModelFormat;
 use App\Exception\ValidationFailedException;
 use Saia\controllers\distribucion\DistributionExecutor;
 use Saia\models\documento\Documento;
-use Saia\models\radicacion_masiva\PaqueteDocumento;
-use Saia\core\model\ModelFormat;
-use Saia\models\ruta\RutaFormato;
-
 
 class FtPqrProperties extends ModelFormat
 {
@@ -17,112 +14,87 @@ class FtPqrProperties extends ModelFormat
     public bool $isPDF = false;
 
     /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     protected function defaultDbAttributes(): array
     {
         return [
             'safe' => [
-                'sys_tipo_json',
-				'nivel_de_urgenc_json',
-				'idft_pqr',
-				'firma',
-				'encabezado',
-				'documento_iddocumento',
-				'sys_fecha_terminado',
-				'sys_fecha_vencimiento',
-				'radicacion',
-				'sys_frecuencia',
-				'sys_anonimo',
-				'sys_impacto',
-				'sys_oportuno',
-				'sys_severidad',
-				'sys_tercero',
-				'sys_estado',
-				'dependencia',
-				'sys_tipo',
-				'fecha_del_event',
-				'nombre',
-				'n_mero_de_ident',
-				'sys_email',
-				'ciudad',
-				'edad',
-				'ciudad_1',
-				'sys_dependencia',
-				'descripcion_1',
-				'nivel_de_urgenc',
-				'sys_folios',
-				'sys_anexos',
-				'sys_tratamiento',
-				'distribucion',
-				'destino_interno',
-				'select_mensajeria',
-				'descripcion',
-				'colilla',
-				'digitalizacion'
+                'documento_iddocumento',
+                'encabezado',
+                'firma',
+                'idft_pqr',
+                'sys_tercero',
+                'sys_severidad',
+                'sys_oportuno',
+                'sys_impacto',
+                'radicacion',
+                'sys_frecuencia',
+                'sys_fecha_vencimiento',
+                'sys_anonimo',
+                'sys_fecha_terminado',
+                'sys_estado',
+                'dependencia',
+                'sys_tipo',
+                'sys_email',
+                'sys_folios',
+                'sys_anexos',
+                'distribucion',
+                'destino_interno',
+                'select_mensajeria',
+                'descripcion',
+                'colilla',
+                'digitalizacion'
             ],
-            'date' => ['sys_fecha_terminado',
-				'sys_fecha_vencimiento',
-				'fecha_del_event'],
+            'date' => ['sys_fecha_vencimiento', 'sys_fecha_terminado'],
             'table' => 'ft_pqr',
             'primary' => 'idft_pqr'
         ];
     }
 
-    public function defaultDocumentRoute(): bool
+    /**
+     * @inheritDoc
+     */
+    public function afterRad(): bool
     {
-        RutaFormato::addDefaultRouteFormat(
-            $this->getFormat()->getPK(),
-            $this->getDocument()->getPK()
-        );
+        $this->createTaskFromDataTemp();
+        if (!$this->radicacion_rapida) {
+            $this->postDocumentRad();
+            if (!$this->getDocument()->belongsToPackage()) {
+                if (!$this->sendDocumentsByEmail()) {
+                    throw new ValidationFailedException('No fue posible enviar la notificacion por correo');
+                }
+            }
+        }
 
         return true;
     }
 
     /**
-* @inheritDoc
-*/
-public function afterRad(): bool
-{
-    $this->createTaskFromDataTemp();
-    if (!$this->radicacion_rapida) {
-        $this->postDocumentRad();
-        if (!$this->getDocument()->belongsToPackage()) {
+     * @inheritDoc
+     */
+    public function afterEdit(): bool
+    {
+        $Documento = $this->getDocument();
+
+        if (!$this->editDistribution()) {
+            throw new ValidationFailedException('No fue posible editar la distribución');
+        }
+
+        if (
+            $Documento->isStarted() &&
+            $this->getFormat()->isAutoApprove()
+        ) {
+            $Documento->estado = Documento::APROBADO;
+            $Documento->estado_aprobacion = Documento::APROBADO_LABEL;
+            $Documento->save();
+            $Documento->getPdfJson(true);
+
             if (!$this->sendDocumentsByEmail()) {
                 throw new ValidationFailedException('No fue posible enviar la notificacion por correo');
             }
         }
 
+        return true;
     }
-
-    return true;
-}
-
-/**
-* @inheritDoc
-*/
-public function afterEdit(): bool
-{
-     $Documento = $this->getDocument();
-
-    if (!$this->editDistribution()){
-          throw new ValidationFailedException('No fue posible editar la distribución');
-    }
-
-    if (
-        $Documento->isStarted() &&
-        $this->getFormat()->isAutoApprove()
-    ) {
-        $Documento->estado = Documento::APROBADO;
-        $Documento->estado_aprobacion = Documento::APROBADO_LABEL;
-        $Documento->save();
-        $Documento->getPdfJson(true);
-
-        if (!$this->sendDocumentsByEmail()) {
-            throw new ValidationFailedException('No fue posible enviar la notificacion por correo');
-        }
-    }
-
-    return true;
-}
 }
--- formatos/pqr_calificacion/FtPqrCalificacionProperties.php
+++ formatos/pqr_calificacion/FtPqrCalificacionProperties.php
@@ -2,32 +2,27 @@
 
 namespace App\Bundles\pqr\formatos\pqr_calificacion;
 
-
 use Saia\core\model\ModelFormat;
-use Saia\models\ruta\RutaFormato;
-
 
 class FtPqrCalificacionProperties extends ModelFormat
 {
-    
-
     public bool $isPDF = false;
 
     /**
-    * @inheritDoc
-    */
+     * @inheritDoc
+     */
     protected function defaultDbAttributes(): array
     {
         return [
             'safe' => [
                 'documento_iddocumento',
-				'encabezado',
-				'firma',
-				'idft_pqr_calificacion',
-				'dependencia',
-				'ft_pqr_respuesta',
-				'experiencia_gestion',
-				'experiencia_servicio'
+                'encabezado',
+                'firma',
+                'idft_pqr_calificacion',
+                'dependencia',
+                'ft_pqr_respuesta',
+                'experiencia_gestion',
+                'experiencia_servicio'
             ],
             'date' => [],
             'table' => 'ft_pqr_calificacion',
@@ -35,23 +30,13 @@ class FtPqrCalificacionProperties extends ModelFormat
         ];
     }
 
-    public function defaultDocumentRoute(): bool
+    /**
+     * @inheritDoc
+     */
+    public function afterRad(): bool
     {
-        RutaFormato::addDefaultRouteFormat(
-            $this->getFormat()->getPK(),
-            $this->getDocument()->getPK()
-        );
+        $this->createTaskFromDataTemp();
 
         return true;
     }
-
-    /**
-* @inheritDoc
-*/
-public function afterRad(): bool
-{
-    $this->createTaskFromDataTemp();
-
-    return true;
-}
 }
